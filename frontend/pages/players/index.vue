c615f984983fe55f95583c498552d302d7dd80cb
Krzysztof Biedroń <arkendil@gmail.com>
2020-02-23 12:57:55 +0100

    Add player filtering in the ranking

--- frontend/pages/players/index.vue
+++ frontend/pages/players/index.vue
@@ -10,19 +10,28 @@
       <v-data-table
         v-if="!loading"
         :headers="headers"
-        :items="players"
+        :items="playersFiltered"
         :footer-props="{
           'items-per-page-options': [10, 30, 50],
         }"
         :items-per-page="30"
-        sort-by="rank_safe"
+        sort-by="trueskill"
         :sort-desc="true"
         class="elevation-1"
       >
         <template v-slot:item.rank="{ item }">
           <span>{{ parseFloat(item.rank).toFixed(3) }}</span>
         </template>
+        <template v-slot:item.trueskill="{ item }">
+          <span>{{ parseFloat(item.trueskill).toFixed(3) }}</span>
+        </template>
       </v-data-table>
+      <v-checkbox
+        v-model="showAll"
+        color="white"
+        class="mx-2"
+        label="Show All"
+      />
     </v-flex>
   </v-layout>
 </template>
@@ -33,6 +42,7 @@ export default {
   data() {
     return {
       loading: false,
+      showAll: false,
       headers: [
         {
           text: 'Player Name',
@@ -50,23 +60,30 @@ export default {
           text: 'TrueSkill (μ - 3σ)',
           align: 'left',
           sortable: true,
-          value: 'rank_safe',
+          value: 'trueskill',
         },
       ],
       players: [],
     };
   },
+  computed: {
+    playersFiltered() {
+      return this.players
+        .filter(
+          ({ rank, sigma }) =>
+            this.showAll || (rank != 25 && sigma < 8),
+        )
+        .map(({ player_id, name, rank, sigma }) => ({
+          player_id,
+          name,
+          rank,
+          trueskill: rank - 3 * sigma,
+        }));
+    },
+  },
   async mounted() {
     this.loading = true;
-    const players = await this.getPlayers();
-    this.players = players.map(
-      ({ player_id, name, rank, sigma }) => ({
-        player_id,
-        name,
-        rank,
-        rank_safe: rank - 3 * sigma,
-      }),
-    );
+    this.players = await this.getPlayers();
     this.loading = false;
   },
 };
